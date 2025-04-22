022db0fcd129078f04f2bb48d04d15363faf3395
Hen <henyael91@gmail.com>
2025-04-22 12:10:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+class Meal{
+string Name {get;set;}
+int Calories {get;set;}
+bool IsVegetarian {get;set;}
+
+
+public Meal (string name, int calories, bool isVegetarian){
+    Name=name;
+    Calories=calories;
+    isVegetarian=IsVegetarian;
+}
+}
+class MealService {
+static async Task<Meal> GetMealAsync(string name){ 
+await Task.Delay(2000);
+return new Meal(name,1000,true);
+
+}
+
+}
+class Program {
+static async Task Main(Meal Meal){ 
+Console.WriteLine("Hey, welcome to the game, can you please enter the name of the meal");
+string mealname=Console.ReadLine();
+MealService ourMealService=await MealService.GetMealAsync(mealname);;
+Console.WriteLine("The meal details are:");
+Console.WriteLine($"Name:{ourMealService.Name}");
+Console.WriteLine($"calories:{ourMealService.calories}");
+Console.WriteLine($"isVegetarian:{ourMealService.isVegetarian}");
+}
+}
+}
+
